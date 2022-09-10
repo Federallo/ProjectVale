b9d834214e84498bb2e33b2ae9a146e20933699b
Andrea <aligiassioma72@gmail.com>
2022-09-10 22:18:12 +0200

    removed other todos

--- src/DataLoader.cpp
+++ src/DataLoader.cpp
@@ -3,15 +3,15 @@
 void DataLoader::readCSV(std::string filename, std::vector<std::unique_ptr<Eigen::RowVectorXf>>& data)
 {
     data.clear();
-    std::ifstream file(filename);//TODO what is ifstream
+    std::ifstream file(filename);
     std::string line, word;
     //determine number of columns in file
-    std::getline(file, line, '\n');//TODO what getline does?
-    std::stringstream ss(line);//TODO what is stringstream
+    std::getline(file, line, '\n');
+    std::stringstream ss(line);
     std::vector<float> parsed_vec;
     while(std::getline(ss, word, ','))//FIXME ", "
     {
-        parsed_vec.push_back(float(std::stof(&word[0])));//TODO what is stof
+        parsed_vec.push_back(float(std::stof(&word[0])));
     }
     uint cols = parsed_vec.size();
     data.push_back(std::make_unique<Eigen::RowVectorXf>(cols));
@@ -43,7 +43,7 @@ void DataLoader::generateData(std::string filename)
     std::ofstream file2(filename + "-out");
     for(uint i = 0; i < 1000; i++)
     {
-        float x = std::rand()/float(RAND_MAX);//TODO what is RAND_MAX?
+        float x = std::rand()/float(RAND_MAX);
         float y = std::rand()/float(RAND_MAX);
         file1 << x << ", " << y << std::endl;
         file2 << 2*x + 10 + y << std::endl;
